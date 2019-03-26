5f9f80d56ae43c0b822df64b0447149ba2171654
SJ <sarah.long4@uoit.net>
2019-03-26 19:35:58 -0400

    Add files via upload

---
+++ Su-Dope-Ku.js
@@ -0,0 +1,9 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+  document.getElementById("gameBoard").style.display = "none";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+  document.getElementById("gameBoard").style.display = "block";
+}
